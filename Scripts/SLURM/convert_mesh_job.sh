f2e9885922fdcf612eaaaf0ac4f7afc29f0651c6
Parm345 <parmghai@protonmail.com>
2026-03-11 02:36:40 -0400

    better decomp and run script

--- Scripts/SLURM/convert_mesh_job.sh
+++ Scripts/SLURM/convert_mesh_job.sh
@@ -5,12 +5,12 @@
 #SBATCH --nodes=1                # number of nodes
 #SBATCH --mem-per-cpu=4g           # memory per cpu
 
-#SBATCH --time=0-00:05             # walltime dd-hh-mm
+#SBATCH --time=0-00:15             # walltime dd-hh-mm
 #SBATCH --output=job%j.txt         # output file
 #SBATCH --mail-type=FAIL
 
 module load openfoam
 
-mesh_path='/home/parmghai/links/scratch/MIE498/first_attempt/MIE498_OpenFOAM/3d_v0.msh'
+mesh_path='/home/parmghai/links/scratch/MIE498/final_data/simulation/turbulence_model_1/medium_mesh/ts005/medium_mesh.msh'
 
 fluent3DMeshToFoam $mesh_path
--- Scripts/SLURM/run.sh
+++ Scripts/SLURM/run.sh
@@ -1,19 +1,20 @@
 #!/bin/bash
-#SBATCH --job-name="openfoam_cube_test"      # job name
+#SBATCH --job-name="openfoam_cube_med_ts0001"      # job name
 
-#SBATCH --ntasks=8                # number of processors
+#SBATCH --ntasks=16                # number of processors
 #SBATCH --nodes=1                # number of nodes
 #SBATCH --mem-per-cpu=4g           # memory per cpu
 
-#SBATCH --time=0-10:00             # walltime dd-hh-mm
+#SBATCH --time=1-00:00             # walltime dd-hh-mm
 #SBATCH --output=cube_test_%j.txt         # output file
-#SBATCH --mail-type=FAIL
+#SBATCH --mail-user=email@mail.ca
+#SBATCH --mail-type=ALL
 
 module load openfoam
 
-projectPath='/home/parmghai/links/scratch/MIE498/first_attempt/MIE498_OpenFOAM'
+projectPath='/home/parmghai/links/scratch/MIE498/final_data/simulation/turbulence_model_1/mdeium_mesh/ts0001'
 
 cd $projectPath
 decomposePar -force | tee log.decompose
-mpirun -np 8 foamRun -solver incompressibleFluid -parallel
+mpirun -np 16 foamRun -solver incompressibleFluid -parallel
 reconstructPar
